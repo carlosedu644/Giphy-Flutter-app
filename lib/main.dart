50afda006e5317457a8334d7afe0a5f312c3581d
Carlos Veron <carloseduardo644@gmail.com>
2019-11-12 15:47:14 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:gifs/ui/home_page.dart';
 
-String api =
-    'https://api.giphy.com/v1/gifs/trending?api_key=U4ZRGvIj2hClqhn1KQY5BemyIzEq1iTs&limit=20&rating=G';
-String searchapi =
-    'https://api.giphy.com/v1/gifs/search?api_key=U4ZRGvIj2hClqhn1KQY5BemyIzEq1iTs&q=dogs&limit=25&offset=&rating=G&lang=en';
+
 void main() {
   runApp(MaterialApp(
     home: HomePage(),
